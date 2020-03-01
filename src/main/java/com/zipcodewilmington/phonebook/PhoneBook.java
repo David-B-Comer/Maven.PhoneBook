d898791fe4bcb9bf0d804193f007891fb7cac2e1
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-03-01 15:53:11 -0500

    done

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,63 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    public Map<String, List<String>> phoneBook;
+
     public PhoneBook(Map<String, List<String>> map) {
+        phoneBook = map;
     }
 
     public PhoneBook() {
-        this(null);
+
+        phoneBook = new LinkedHashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+        ArrayList<String> number = new ArrayList<>(Arrays.asList(phoneNumber));
+        phoneBook.put(name, number);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        ArrayList<String> number = new ArrayList<>(Arrays.asList(phoneNumbers));
+        phoneBook.put(name, number);
     }
 
     public void remove(String name) {
+        phoneBook.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        ArrayList<String> number = new ArrayList<>(Arrays.asList(name));
+        return phoneBook.containsKey(name) || phoneBook.containsValue(number);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return phoneBook.get(name);
     }
 
-    public String reverseLookup(String phoneNumber)  {
-        return null;
+    public String reverseLookup(String phoneNumber) {
+        for (Map.Entry<String, List<String>> entry : phoneBook.entrySet()) {
+            if (entry.getValue().contains(phoneNumber)) {
+                return entry.getKey();
+            }
+        }
+            return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        ArrayList<String> names = new ArrayList<>();
+        for (String name : phoneBook.keySet()) {
+            names.add(name);
+        }
+        return names;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return phoneBook;
     }
 }
